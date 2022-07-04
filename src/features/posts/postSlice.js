12e4cc714f8515f2f85f2db26f66e764b504f39a
Quadatz <quadath@gmail.com>
2022-07-04 22:02:07 +0300

    post date

--- src/features/posts/postSlice.js
+++ src/features/posts/postSlice.js
@@ -1,8 +1,9 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { sub } from "date-fns";
 
 const initialState = [
-    { id: '1', title: 'Learning Redux Toolkit', content: "I 've heard good thing"},
-    { id: '2', title: 'Slices...', content: "The more i say Slice, the more i"}
+    { id: '1', title: 'Learning Redux Toolkit', content: "I 've heard good thing", date: sub(new Date(), { minutes: 10 }).toISOString()},
+    { id: '2', title: 'Slices...', content: "The more i say Slice, the more i", date: sub(new Date(), { minutes: 15 }).toISOString()}
 ]
 
 const postsSlice = createSlice({
@@ -19,6 +20,7 @@ const postsSlice = createSlice({
                         id: nanoid(),
                         title, 
                         content,
+                        date: new Date().toISOString(),
                         userId
                     }
                 }
